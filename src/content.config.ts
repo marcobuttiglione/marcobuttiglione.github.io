abbac132bddcf127bd5cddfca11fa613bdeb2fc2
Marco Domenico Buttiglione <marco.buttiglione98@gmail.com>
2025-12-16 13:12:54 +0100

    Remove old files

--- src/content.config.ts
+++ src/content.config.ts
@@ -1,21 +1,6 @@
 import { defineCollection, z } from 'astro:content';
 import { glob } from 'astro/loaders';
 
-const blog = defineCollection({
-	// Load Markdown and MDX files in the `src/content/blog/` directory.
-	loader: glob({ base: './src/content/blog', pattern: '**/*.{md,mdx}' }),
-	// Type-check frontmatter using a schema
-	schema: ({ image }) =>
-		z.object({
-			title: z.string(),
-			description: z.string(),
-			// Transform string to Date object
-			pubDate: z.coerce.date(),
-			updatedDate: z.coerce.date().optional(),
-			heroImage: image().optional(),
-		}),
-});
-
 const projects = defineCollection({
 	loader: glob({ base: './src/content/projects', pattern: '**/*.{md,mdx}' }),
 	schema: ({ image }) =>
@@ -32,4 +17,4 @@ const projects = defineCollection({
 		}),
 });
 
-export const collections = { blog, projects };
+export const collections = { projects };
